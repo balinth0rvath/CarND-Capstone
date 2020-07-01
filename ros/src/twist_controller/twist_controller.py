7a581f9b3e8f38c79c075c98a9da8e6aa0c70509
Balint Horvath <horvath0balint@gmail.com>
2020-07-01 20:08:55 +0200

    Fix twist controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -41,13 +41,14 @@ class Controller(object):
         
         current_vel = self.vel_lpf.filt(current_vel)
         
-        rospy.logwarn("Angular velocity: {0}".format(angular_vel))
-        rospy.logwarn("Target velocity: {0}".format(linear_vel))
-        rospy.logwarn("Target angular velocity: {0}\n".format(angular_vel))
-        rospy.logwarn("Current velocity: {0}".format(current_vel))
-        rospy.logwarn("Filtered velocity: {0}".format(self.vel_lpf.get()))
+        #rospy.logwarn("Angular velocity: {0}".format(angular_vel))
+        #rospy.logwarn("Target velocity: {0}".format(linear_vel))
+        #rospy.logwarn("Target angular velocity: {0}\n".format(angular_vel))
+        #rospy.logwarn("Current velocity: {0}".format(current_vel))
+        #rospy.logwarn("Filtered velocity: {0}".format(self.vel_lpf.get()))
                         
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+                
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
         
